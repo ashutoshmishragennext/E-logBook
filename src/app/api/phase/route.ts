03b928ad685eff6dd8d40352fd0023bce5488af8
Ashutosh kumar mishra <ashutoshmishra8796@gmail.com>
2025-03-29 12:56:09 +0530

    add route

--- src/app/api/phase/route.ts
+++ src/app/api/phase/route.ts
@@ -1,12 +1,20 @@
 import { db } from "@/db";
 import { PhaseTable } from "@/db/schema";
-import { NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
 
 
-export async function GET(){
+export async function GET(req :NextRequest){
+
+    const searchParams = req.nextUrl.searchParams;
+    const Ay = searchParams.get('academicYears');
 
     try {
-        const phase= await db.select().from(PhaseTable)
+        let phase= await db.select().from(PhaseTable)
+
+        if(Ay){
+            phase= await db.select().from(PhaseTable).where(eq(PhaseTable.academicYearId,Ay))
+        }
         return NextResponse.json(phase)
         
     } catch (error) {
--- src/app/api/subject/route.ts
+++ src/app/api/subject/route.ts
@@ -1,10 +1,16 @@
 import { db } from "@/db";
 import { SubjectTable } from "@/db/schema";
-import { NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
 
- export async function GET(){
+ export async function GET(req : NextRequest){
     try {
-        const subject = await db.select().from(SubjectTable)
+        const searchParams = req.nextUrl.searchParams;
+        const PhaseId = searchParams.get('PhaseId');
+        let subject = await db.select().from(SubjectTable)
+        if(PhaseId){
+             subject= await db.select().from(SubjectTable).where(eq(SubjectTable.phaseId,PhaseId))
+        }
         return NextResponse.json(subject)
     } catch (error) {
         return NextResponse.json({
